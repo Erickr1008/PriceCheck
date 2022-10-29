5d1419eb50662bd69095e8fd2b9201e93cc0b8a5
Dae17 <102134738+Dae17@users.noreply.github.com>
2022-10-28 22:52:13 -0700

    Add files via upload

---
+++ sales-site/apps/listing/models.py
@@ -0,0 +1,20 @@
+# Create your models here.
+
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class item(models.Model):
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    item_name = models.CharField(max_length=120)
+    price = models.IntegerField()
+    item_discription = models.TextField()
+    photo = models.ImageField(upload_to='items')
+
+    def get_absolute_url(self):
+        return reverse('apps:listing:detail', args=(self.pk, ))
+
+    def __str__(self) -> str:
+        return self.item_name
