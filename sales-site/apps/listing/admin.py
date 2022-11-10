edaf8a978b292ad0a74c676d1460a1adf0f81055
Erickr1008 <112650321+Erickr1008@users.noreply.github.com>
2022-11-10 10:19:32 -0800

    Delete sales-site directory

--- sales-site/apps/listing/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import item
-
-# Register your models here.
-@admin.register(item)
-class ItemAdmin(admin.ModelAdmin):
-    pass
--- sales-site/apps/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-app_name = "apps"
-
-urlpatterns = [
-    path("", include("apps.listing.urls")),
-    path("users/", include ("apps.users.urls"))
-]
